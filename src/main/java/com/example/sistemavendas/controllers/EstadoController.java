a034df99a21da192ed0839cd69d9bb9374c39533
Pedro Barbosa <phdfrocha99@gmail.com>
2025-03-29 22:56:32 -0300

    Listagem, Remoção e edição de Estado

--- src/main/java/com/example/sistemavendas/controllers/EstadoController.java
+++ src/main/java/com/example/sistemavendas/controllers/EstadoController.java
@@ -4,9 +4,13 @@ import com.example.sistemavendas.models.Estado;
 import com.example.sistemavendas.repository.EstadoRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 public class EstadoController {
@@ -28,4 +32,26 @@ public class EstadoController {
         estadoRepositories.saveAndFlush(estado);
         return cadastrarEstado(new Estado());
     }
+    @GetMapping("/editarEstado/{id}")
+    public ModelAndView editarEstado(@PathVariable Long id) {
+        Optional<Estado> estado = estadoRepositories.findById(id);
+        return cadastrarEstado(estado.get());
+    }
+
+    @GetMapping("/removerEstado/{id}")
+    public ModelAndView removerEstado(@PathVariable Long id) {
+        Optional<Estado> estado = estadoRepositories.findById(id);
+        estadoRepositories.delete(estado.get());
+        return listarEstados();
+    }
+
+
+
+    @GetMapping("/listarEstados")
+    public ModelAndView listarEstados() {
+        ModelAndView mv = new ModelAndView("administrativo/estado/lista");
+        mv.addObject("listaEstados", estadoRepositories.findAll());
+        return mv;
+    }
+
 }
--- src/main/java/com/example/sistemavendas/repository/EstadoRepositories.java
+++ src/main/java/com/example/sistemavendas/repository/EstadoRepositories.java
@@ -4,4 +4,5 @@ import com.example.sistemavendas.models.Estado;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EstadoRepositories extends JpaRepository<Estado, Long> {
+    Long id(Long id);
 }
